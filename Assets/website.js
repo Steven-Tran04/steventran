aca8c3664f3124f6c02a058131fcbf2a62337a72
Steven Tran <steventran@Stevens-MacBook-Pro-6.local>
2023-12-08 23:21:26 -0800

    Project and Experience Updates

--- Assets/website.js
+++ Assets/website.js
@@ -33,3 +33,27 @@ function addObserver(el, options){
 scrollTrigger('.scroll-reveal', {
     rootMargin: '-200px',
 })
+
+// fetch and render projects section (implement later when actually have projects :')
+// const projects = document.querySelector("#projects .contents")
+// const renderProjects = async () => {
+//     try {
+//         const data = await fetch("data/projects.json");
+//         const response = await data.json();
+//         let item = "";
+//         for (let i = 0; i < response.length; i++) {
+//             item += `
+//             <div class="product">
+//              <img src=${response[i].thumbnail.url} alt=${response[i].name} />
+//              <div class="text">
+//               <h3>${response[i].name}</h3>
+//               <a target="blank" href=${response[i].hoster_url} >View code on ${response[i].hoster_url}</a>
+//               <a target="blank" href=${response[i].demo_url} >Demo</a>
+//              </div>
+//             </div>`
+//         }
+//         projects.innerHTML = item;
+//     } catch {error} {
+//         console.log("projects error ==>", error);
+//     }
+// };
